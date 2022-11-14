40adc2e51822b2a1d3fd7cfc6158588b65143917
VerbitskiA <eternity.2019@mail.ru>
2022-11-14 18:12:02 +0300

    Начало разработки

---
+++ SocialNetwork/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SocialNetwork.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private List<User> _users = new List<User>()
+        {
+            new User()
+            {
+                Id = 1,
+                Name = "Mike"
+            },
+            new User()
+            {
+                Id = 2,
+                Name = "Alice"
+            },
+            new User()
+            {
+                Id = 3,
+                Name = "Alex"
+            }
+        };
+
+        [HttpGet]
+        [Route("count")]
+        public async Task<ActionResult<int>> GetUserCountAsync()
+        {
+            await Task.Delay(3000);
+
+            return Ok(_users.Count);
+        }
+    }
+}
---
+++ SocialNetwork/User.cs
@@ -0,0 +1,11 @@
+﻿namespace SocialNetwork
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+    }
+}
