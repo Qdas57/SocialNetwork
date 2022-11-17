7353d1151fbebcdc04a4170bbb627f0583b14519
Admin <Admin@DESKTOP-NKL4TCH>
2022-11-17 17:13:42 +0300

    добавление нового юзера. проверка на наличие этого юзера

--- SocialNetwork.Services/Repositories/UserRepository.cs
+++ SocialNetwork.Services/Repositories/UserRepository.cs
@@ -1,4 +1,6 @@
-﻿using SocialNetwork.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNetwork.Data;
+using SocialNetwork.Data.Entities;
 
 namespace SocialNetwork.Services.Repositories
 {
@@ -14,9 +16,34 @@ namespace SocialNetwork.Services.Repositories
         public async Task<bool> RegisterUserAsync(string firstName, string lastName, string email, string password,
         string? phone, string? avatar, DateTime birthDate)
         {
-            //TODO: реализоавть добавление пользователя
+            {
+
+                var user = new UserEntity
+                {
+                    UserId = Guid.NewGuid(),
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email,
+                    Password = password,
+                    Phone = phone,
+                    Avatar = avatar,
+                    BirthDate = birthDate
+                };
+
+                await _db.Users.AddAsync(user);
+                await _db.SaveChangesAsync();
+            }
 
             return true;
+        }
+
+        public async Task<bool> IsUserExistAsync(string email)
+        {
+            if (await _db.Users.FirstOrDefaultAsync(u => u.Email == email) != null)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
